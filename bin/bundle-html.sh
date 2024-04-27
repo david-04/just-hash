4bbda0ea748826c48886761a61b5a257605153d7
David Hofmann <65802756+david-04@users.noreply.github.com>
2024-04-28 08:34:56 +1000

    Inline the base64 favicon automatically during the build

--- bin/bundle-html.sh
+++ bin/bundle-html.sh
@@ -4,18 +4,12 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 echo Building docs...
+
 mkdir -p ../docs
-{
-    awk '/just-hash\.css/ { exit(0) } { print }' ../dist/just-hash.html
-    echo '<style type="text/css">'
-    cat ../dist/just-hash.css
-    echo '</style>'
-    awk 'active { if (/just-hash.js/) { exit(0) } print } /just-hash\.css/ { active=1 } ' ../dist/just-hash.html
-    echo '<script type="text/javascript">'
-    cat ../dist/just-hash.js
-    echo '</script>'
-    awk 'active { print } /just-hash\.js/ { active=1 }' ../dist/just-hash.html
 
-} >../docs/index.html.tmp
-
-mv -f ../docs/index.html.tmp ../docs/index.html
+# shellcheck disable=SC2002
+cat ../dist/just-hash.html |
+    awk '{if (/just-hash.css/) { print "<style type=\\"text/css\\">"; system("cat ../dist/just-hash.css"); print "</style>"} else {print}}' |
+    awk '{if (/just-hash.js/) { print "<script type=\\"text/javascript\\">"; system("cat ../dist/just-hash.js"); print "</script>"} else {print}}' |
+    awk '{if (/favicon.svg/) { printf("%s","<link rel=\\"icon\\" href=\\"data:image/svg+xml;base64,"); system("base64 --wrap=0 ../dist/favicon.svg"); print "\\"/>"} else {print}}' \
+        >../docs/index.html
